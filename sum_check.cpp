fd70a8691046cbc668d48a62c769a2997aeca92c
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-06-20 08:09:38 +0000

    check if the sum of any two numbers in a vector are equal to a given value.

---
+++ sum_check.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+class check{
+public:
+    void checksum(int value, vector <int> & a){
+        map <int , int> m;
+        for (int i:a){
+            if (m[value-i]>0){
+                cout<<"TRUE!\n";
+                return;
+            }
+            else {m[i]+=1;}
+        }
+        cout<<"FALSE!\n";
+    }
+};
+int main(){
+    vector <int> a = {1,2,5,7,8,4,56};
+    int value = 64;
+    check solution;
+    solution.checksum(value,a);
+    return 0;
+}
