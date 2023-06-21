f2561f0142ddc5f735c19a9631a7e69c8b2eb0e6
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-06-21 04:49:01 +0000

    x^power code (int x, int power)

---
+++ num_power/calculate_power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class power{
+public:
+    double num_power(int x , int power){
+        double ans = 1.0 , y = x;
+        if (power<0){y = 1/y;}
+        if (power != 0){
+            for (int i=1;i<=abs(power);++i){
+                ans *= y;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    power p;
+    cout<<p.num_power(-2,3)<<endl;
+}
