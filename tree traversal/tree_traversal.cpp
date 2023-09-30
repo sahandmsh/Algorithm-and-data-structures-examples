33d6a62c0dee73e51058b3df058a2ea519938d06
Sahand Mosharafian <82970651+sahandmsh@users.noreply.github.com>
2023-09-30 13:45:19 -0700

    Update tree_traversal.cpp

--- tree traversal/tree_traversal.cpp
+++ tree traversal/tree_traversal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 struct Tree{
     int value;
@@ -32,6 +33,31 @@ public:
             cout<<t->value<<endl;
         }
     }
+
+    vector<vector<int>> bfs(Tree* root){
+        vector<vector<int>> ans;
+        if (!root) return ans;
+        queue <TreeNode*> nodes_q;
+        nodes_q.push(root);
+        nodes_q.push(NULL);
+        vector <int> single_level;
+        while (!nodes_q.empty()){
+            TreeNode* node = nodes_q.front();
+            nodes_q.pop();
+            if (node == NULL){
+                ans.push_back(single_level);
+                single_level.resize(0);
+                if (nodes_q.size()!=0) nodes_q.push(NULL);
+            }
+            else{
+                single_level.push_back(node->val);
+                if(node->left) nodes_q.push(node->left);
+                if(node->right) nodes_q.push(node->right);
+            }
+
+        }
+        return ans;
+    }
 };
 
 int main(){
@@ -47,4 +73,10 @@ int main(){
     x.inorder(t);
     cout << "postorder traversal:\n";
     x.postorder(t);
+    cout << "level order traversal (BFS):";
+    vector<vector<int>> ans = x.bfs(t);
+    for (int i=0;i<ans.size();++i){
+        cout<<"\n Tree level "<<(i+1)<<" nodes:\n";
+        for (auto j:ans[i]) cout<<j<<", ";
+    }
 }
